5dc33ed5116c81617b17153754a5f88964eb55ed
amolmane7471 <maneamol3233@gmail.com>
2022-09-10 11:12:24 +0530

    [Amol] : added UC 1.4 print each elements in double

--- src/com/bridgelabz/lambdaexpression/NumberPlayList.java
+++ src/com/bridgelabz/lambdaexpression/NumberPlayList.java
@@ -3,51 +3,54 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class NumberPlayList{
-	
 	public static void main(String[] args) {
-		
+
 		List<Integer> myNumberList = new ArrayList<Integer>();
-		for(int index=0 ; index<5 ; index++)
+		for (int index = 0; index < 5; index++)
 			myNumberList.add(index);
-		
-		//Method 1 : Traversing  using Iterator
+
+		// Method 1 : Traversing using Iterator
 		Iterator<Integer> iterator = myNumberList.iterator();
-		while(iterator.hasNext()) {
+		while (iterator.hasNext()) {
 			Integer eachInteger = iterator.next();
-			System.out.println("Iterator Value: "+eachInteger);
+			System.out.println("Iterator Value: " + eachInteger);
 		}
-		
-		
-		//Method 2 : Traversing with Consumer interface 
-		class MyConsumer implements Consumer<Integer>{
+
+		// Method 2 : Traversing with Consumer interface
+		class MyConsumer implements Consumer<Integer> {
 			public void accept(Integer t) {
-				System.out.println("Method2 : Consumer Impl Value: "+t);
+				System.out.println("Method2 : Consumer Impl Value: " + t);
 			}
 		}
 		MyConsumer action = new MyConsumer();
 		myNumberList.forEach(action);
-		
-		
-		//Method 3 : Traversing with Anonymous Consumer interface 
-		myNumberList.forEach(new Consumer<Integer>(){
+
+		// Method 3 : Traversing with Anonymous Consumer interface
+		myNumberList.forEach(new Consumer<Integer>() {
 			public void accept(Integer t) {
-				System.out.println("Method3 : forEach anonymous Impl Value: "+t);
+				System.out.println("Method3 : forEach anonymous Impl Value: " + t);
 			}
 		});
-		
-		
-		//Method 4: Explicit Lambda Function
+
+		// Method 4: Explicit Lambda Function
 		Consumer<Integer> myListAction = n -> {
-			System.out.println("Method4 : forEach Explicit Lambda Impl Value: "+n);
+			System.out.println("Method4 : forEach Explicit Lambda Impl Value: " + n);
 		};
 		myNumberList.forEach(myListAction);
-		
-		
-		//Method 5: Implicit Lambda Function
-		myNumberList.forEach(n ->{
-			System.out.println("Method5 : forEach Implicit Lambda Impl Value: "+n);
+
+		// Method 5: Implicit Lambda Function
+		myNumberList.forEach(n -> {
+			System.out.println("Method5 : forEach Implicit Lambda Impl Value: " + n);
 		});
+
+		// Method 6: Implicit Lambda Function To Print Double Value
+		Function<Integer, Double> toDoubleFunction = Integer::doubleValue;
+		myNumberList.forEach(n -> {
+			System.out.println("Mth6 : forEach Value: " + toDoubleFunction.apply(n));
+		});
+
 	}
 }
