b65a8f6db392e106bc0d35135e1456084ca7a090
amolmane7471 <maneamol3233@gmail.com>
2022-09-10 10:59:57 +0530

    [Amol] : added UC 1.2 to show result of math operations

--- src/com/bridgelabz/lambdaexpression/MathFunctionIF.java
+++ src/com/bridgelabz/lambdaexpression/MathFunctionIF.java
@@ -2,4 +2,7 @@ package com.bridgelabz.lambdaexpression;
 
 interface MathFunctionIF{
 	int calculate(int a, int b);
+	static void printResult(int a, int b, String function, MathFunctionIF functionObject) {
+		System.out.println("RESULT OF " +function+" is "+functionObject.calculate(a,b));
+	}
 }
